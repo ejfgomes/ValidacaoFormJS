bd3f371369cc8e2b42a921af5c4a0ca04845a6cb
Erik Juan <e_gomes@live.com>
2015-12-29 22:58:54 -0200

    trocado array por um boolean

--- js/default.js
+++ js/default.js
@@ -10,110 +10,110 @@ function valida() {
         if (elemento.tagName == "INPUT" || elemento.tagName == "SELECT" || elemento.tagName == "TEXTAREA") {
             switch (elemento.getAttribute("name")) {
                 case 'texto':
-                    var elementoComErro = [];
+                    var elementoComErro = false;
                     if (nulo(elemento)) {
                         addErro(elemento);
                         mensagens = addMensagemDeErro(mensagens, "O campo Texto é obrigatório.");
-                        elementoComErro.push(true);
+                        elementoComErro = true;
                     }
 
-                    if (elementoComErro.length == 0) 
+                    if (!elementoComErro) 
                         removeErro(elemento);    
 
                     break;
                 case 'numero':
-                    var elementoComErro = [];
+                    var elementoComErro = false;
                     if (nulo(elemento)) {
                         addErro(elemento);
                         mensagens = addMensagemDeErro(mensagens, "O campo Número é obrigatório.");
-                        elementoComErro.push(true);
+                        elementoComErro = true;
                     }
 
                     if (!numeroEntre(elemento, 10, 20)) {
                         addErro(elemento);
                         mensagens = addMensagemDeErro(mensagens, "O campo Número deve estar entre 10 e 20");
-                        elementoComErro.push(true);
+                        elementoComErro = true;
                     };
 
-                    if (elementoComErro.length == 0) 
+                    if (!elementoComErro) 
                         removeErro(elemento);    
 
                     break;
                 case 'algumaCoisa':
-                    var elementoComErro = [];
+                    var elementoComErro = false;
                 	if (nulo(elemento)) {
                         addErro(elemento);
                         mensagens = addMensagemDeErro(mensagens, "O campo Alguma coisa é obrigatório.");
-                        elementoComErro.push(true);
+                        elementoComErro = true;
                     }
 
                     if (quantidadeDeCaracteresEntre(elemento, 0, 500)) {
                         addErro(elemento);
                         mensagens = addMensagemDeErro(mensagens, "O campo Alguma coisa deve ser menor que 500 caracteres.");
-                        elementoComErro.push(true);
+                        elementoComErro = true;
                     }
 
-                    if (elementoComErro.length == 0) 
+                    if (!elementoComErro) 
                         removeErro(elemento);    
 
                     break;
                 case 'selectOption':
-                    var elementoComErro = [];
+                    var elementoComErro = false;
                     if (nulo(elemento)) {
                         addErro(elemento);
                         mensagens = addMensagemDeErro(mensagens, "O campo SelectOption é obrigatório.");
-                        elementoComErro.push(true);
+                        elementoComErro = true;
                     }
 
-                    if (elementoComErro.length == 0) 
+                    if (!elementoComErro) 
                         removeErro(elemento);    
 
                     break;
                 case 'dtcriacao':
-                    var elementoComErro = [];
+                    var elementoComErro = false;
                     if (nulo(elemento)) {
                         addErro(elemento);
                         mensagens = addMensagemDeErro(mensagens, "O campo Data de criação é obrigatório.");
-                        elementoComErro.push(true);
+                        elementoComErro = true;
                     }
 
-                    if (elementoComErro.length == 0) 
+                    if (!elementoComErro) 
                         removeErro(elemento);    
 
                     break;
                 case 'ativo':
-                    var elementoComErro = [];
+                    var elementoComErro = false;
                     if (!checkado(elemento)) {
                         addErro(elemento);
                         mensagens = addMensagemDeErro(mensagens, "O campo Ativo precisa estar marcado.");
-                        elementoComErro.push(true);
+                        elementoComErro = true;
                     };
 
-                    if (elementoComErro.length == 0)
+                    if (!elementoComErro)
                         removeErro(elemento);
 
                     break;
                 case 'radioList':
-                    var elementoComErro = [];
+                    var elementoComErro = false;
                     if (nulo(elemento)) {
                         addErro(elemento);
                         mensagens = addMensagemDeErro(mensagens, "O campo RadioList é obrigatório.");
-                        elementoComErro.push(true);
+                        elementoComErro = true;
                     }
 
-                    if (elementoComErro.length == 0) 
+                    if (!elementoComErro) 
                         removeErro(elemento);
 
                     break;
                 case 'cor':
-                    var elementoComErro = [];
+                    var elementoComErro = false;
                     if (nulo(elemento)) {
                         addErro(elemento);
                         mensagens = addMensagemDeErro(mensagens, "O campo Cor é obrigatório.");
-                        elementoComErro.push(true);
+                        elementoComErro = true;
                     }
 
-                    if (elementoComErro.length == 0) 
+                    if (!elementoComErro) 
                         removeErro(elemento);
 
                     break;
